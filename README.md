7c85c02952bf5567760324456e2ea3baa17a7134
Pr3acher <Pr3acher777h@gmail.com>
2015-01-02 19:49:40 +0100

    Version 1.1 - see CHANGELOG

---
+++ README.md
@@ -0,0 +1,8 @@
+# color_shell
+color_shell is a simple multi-platform C library that helps you to write some
+colored text into a terminal. It's been tested on GNU/linux and windows for now;
+here are some screenshots :
+
+![color_shell on GNU/linux](https://github.com/Pr3ach/color_shell/raw/master/img/color_shell_linux.png)
+![color_shell on windows](https://github.com/Pr3ach/color_shell/raw/master/img/color_shell_win32.png)
+
--- tests/test.c
+++ tests/test.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "../color_shell.h"
+#include "../src/color_shell.h"
 
 #define LEN(a) sizeof(a)/sizeof(*a)
 
 int main(int argc, char *argv[])
 {
-  void (*call[])(char *, ...) = {w_red, w_green, w_white, w_blue, w_purple, w_yellow, w_cyan};
-  char *args[] = {"red", "green", "white", "blue", "purple", "yellow", "cyan"};
+  void (*call[])(char *, ...) = {w_red, w_bred, w_green, w_bgreen, w_white, \
+    w_bwhite, w_blue, w_bblue, w_purple, w_bpurple, w_yellow, w_byellow, w_cyan, \
+      w_bcyan};
+  char *args[] = {"red", "bold red", "green", "bold green", "white", "bold white", \
+    "blue", "bold blue", "purple", "bold purple", "yellow", "bold yellow", "cyan", \
+      "bold cyan"};
   uint32_t i = 0;
 
   for(i = 0; i<LEN(call); i++)
